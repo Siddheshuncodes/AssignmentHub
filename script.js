6d0fe5b12be2e8cdbbb0a6fc6dae09456869724f
Siddheshuncodes <113630547+Siddheshuncodes@users.noreply.github.com>
2023-05-23 14:30:29 +0530

    updated js file

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-  $(document).ready(function() {
-    $('.navbar-toggle-icon').click(function() {
-      $('.nav-links').slideToggle();
-    });
-
-    $(document).click(function(e) {
-      var target = e.target;
-      if (!$(target).is('.navbar-toggle-icon, .nav-links, .nav-links *')) {
-        $('.nav-links').slideUp();
-      }
-    });
-
-    $('.dropdown > a').click(function(e) {
-      e.preventDefault();
-      $(this).siblings('.dropdown-content').slideToggle();
-    });
+$(document).ready(function() {
+  $('.navbar-toggle-icon').click(function() {
+    $('.nav-links').toggleClass('active');
   });
+
+  $(document).click(function(e) {
+    var target = e.target;
+    if (!$(target).is('.navbar-toggle-icon, .nav-links, .nav-links *')) {
+      $('.nav-links').removeClass('active');
+    }
+  });
+
+  $('.dropdown > a').click(function(e) {
+    e.preventDefault();
+    $(this).siblings('.dropdown-content').slideToggle();
+  });
+});
